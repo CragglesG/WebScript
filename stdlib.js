862995361b420242d876bcb3807c2ac401293c66
Craig Giles <121510295+CragglesG@users.noreply.github.com>
2024-08-06 18:15:33 +0100

    Added axios, started requests feature

--- stdlib.js
+++ stdlib.js
@@ -1,3 +1,5 @@
+import axios from "axios"
+
 export class WebScriptError extends Error {
     constructor(msg) {
         super()
@@ -9,7 +11,31 @@ export class WebScriptError extends Error {
     }
 }
 
+export class Request {
+    constructor(url) {
+        this.url = url
+        this.data
+        this.status
+        this.headers
+    }
+
+    get() {
+        axios.get(this.url)
+            .then(function (response) {
+                this.data = response.data
+                this.status = response.status
+                this.headers = response.headers
+
+            })
+            .catch(function (error) {
+                throw new WebScriptError(`GET Request Error: ${error}`)
+            })
+    }
+    
+}
+
 export default {
+    request: new Request(),
     display: args => console.log(...args),
     random: ([min, max]) => {
         if (min >= 0 && max <= 1) return Math.random()
--- test.ws
+++ test.ws
@@ -4,7 +4,6 @@ prepare array as [1, 0, 3, 5]
 display(array)
 array.add(7)
 display(array)
-display(array.get(1))
 func yay {
     display("well this works")
 }
@@ -17,4 +16,9 @@ prepare array as "oops"
 display(array)
 type S has { x,y,z }
 prep S( x:1, y:2, z:3 )
-display(S)
+display(S())
+
+prepare g as request
+prepare g.url as 'https://google.ie'
+
+g.get()
