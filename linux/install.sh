5c84f6a2af424d2ab902d3c7ac59816a9e2c1c11
Craig Giles <121510295+CragglesG@users.noreply.github.com>
2024-08-08 11:45:31 +0100

    Fixed linux/install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -1,4 +1,4 @@
 mkdir ~/.webscript
-git clone "https://github.com/CragglesG/Easel/" .
-chmod +x webscript
+git clone "https://github.com/CragglesG/Easel/" ~/.webscript
+chmod +x ~/.webscript/webscript
 echo 'PATH=$PATH:$HOME/.webscript' >>~/.bashrc
